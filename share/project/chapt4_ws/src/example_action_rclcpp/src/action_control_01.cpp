08769c45bfa70f8d9b040531a922d50927d33daf
shengfzy <shengfzysk12345@163.com>
2025-07-27 21:44:45 +0000

    Update action_control_01.cpp

--- share/project/chapt4_ws/src/example_action_rclcpp/src/action_control_01.cpp
+++ share/project/chapt4_ws/src/example_action_rclcpp/src/action_control_01.cpp
@@ -4,10 +4,82 @@
 
 class ActionControl01 : public rclcpp::Node {
 public:
-    explicit ActionControl01(std::string name) : Node(name) {
+    using MoveRobot = robot_control_interfaces::action::MoveRobot;
+    using GoalHandleMoveRobot = rclcpp_action::ClientGoalHandle<MoveRobot>;
+
+    explicit ActionControl01(std::string name, const rclcpp::NodeOptions& node_options = rclcpp::NodeOptions()) : Node(name, node_options) {
         RCLCPP_INFO(this->get_logger(), "Node %s has been started.", name.c_str());
+
+        this->client_ptr_ = rclcpp_action::create_client<MoveRobot>(this, "move_robot");
+        this->timer_ = this->create_wall_timer(std::chrono::milliseconds(500),
+                                               std::bind(&ActionControl01::send_goal, this));
+    }
+
+    void send_goal() {
+        using namespace std::placeholders;
+
+        this->timer_->cancel();
+
+        if(!this->client_ptr_->wait_for_action_server(std::chrono::seconds(10))) {
+            RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+
+            rclcpp::shutdown();
+            return;
+        }
+
+        auto goal_msg = MoveRobot::Goal();
+        goal_msg.distance = 10;
+
+        RCLCPP_INFO(this->get_logger(), "Sending goal");
+
+        auto send_goal_options = 
+                rclcpp_action::Client<MoveRobot>::SendGoalOptions();
+        send_goal_options.goal_response_callback = 
+                std::bind(&ActionControl01::goal_response_callback, this, _1);
+        send_goal_options.feedback_callback = 
+                std::bind(&ActionControl01::feedback_callback, this, _1, _2);
+        send_goal_options.result_callback = 
+                std::bind(&ActionControl01::result_callback, this, _1);
+        this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
     }
 private:
+    rclcpp_action::Client<MoveRobot>::SharedPtr client_ptr_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    void goal_response_callback(GoalHandleMoveRobot::SharedPtr goal_handle) {
+        if(!goal_handle) {
+            RCLCPP_INFO(this->get_logger(), "Goal was rejected by server");
+        }
+        else {
+            RCLCPP_INFO(this->get_logger(), "Goal accepted by server, waiting for result");
+        }
+    }
+
+    void feedback_callback(
+        GoalHandleMoveRobot::SharedPtr,
+        const std::shared_ptr<const MoveRobot::Feedback> feedback
+    ) {
+        RCLCPP_INFO(this->get_logger(), "Feedback current pose: %f", feedback->pose);
+    }
+
+    void result_callback(const GoalHandleMoveRobot::WrappedResult& result) {
+        switch(result.code) {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was aborted");
+                return;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_ERROR(this->get_logger(), "Goal was canceled");
+                return;
+            default:
+                RCLCPP_ERROR(this->get_logger(), "Unknown resutl code");
+                return;
+        }
+
+        RCLCPP_INFO(this->get_logger(), "Result received: %f", result.result->pose);
+    }
+    
 };
 
 int main(int argc, char **argv) {
